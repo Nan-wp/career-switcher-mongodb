6a3391cd918681883a4865c1aab780365c78b4c2
Nan-wp <150341587+Nan-wp@users.noreply.github.com>
2024-01-29 16:15:05 +0700

    commit-1

--- module-1-introduction-to-mongodb/exercise-1/index.js
+++ module-1-introduction-to-mongodb/exercise-1/index.js
@@ -1,4 +1,20 @@
 // go to link in google slide
 // create object in variable below
 
-const data = {};
+const data = {
+    Title: string,
+	Rating: string | number,
+	totalRate: string | number,
+	Sold: string | number,
+	Price: string | number,
+	promotionCode: string[''],
+	Wholesale: string,
+    shipping: 
+    { 
+    destination: string, 
+    shippingCost: number
+    },
+    sku: string[''],
+    Images: string[''],
+
+};
